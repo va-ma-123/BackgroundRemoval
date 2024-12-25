85934331b3018cf388020cc7ab69380137068791
Varun Mangla <mangla.8@osu.edu>
2024-12-24 22:45:16 -0500

    homepage almost done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,34 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
+import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import Result from './pages/Result'
+import BuyCredit from './pages/BuyCredit'
+import Navbar from './components/Navbar'
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className='min-h-screen bg-slate-100'>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={ <Home/> } />
+        <Route path='/result' element={ <Result/> } />
+        <Route path='/buy' element={ <BuyCredit/> } />
+      </Routes>
+    </div>
   )
 }
 
---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Header from '../components/Header'
+import Steps from '../components/Steps'
+import Slider from '../components/Slider'
+import Testimonials from '../components/Testimonials'
+
+const Home = () => {
+  return (
+    <div>
+        <Header />
+        <Steps />
+        <Slider />
+        <Testimonials />
+    </div>
+  )
+}
+
+export default Home
